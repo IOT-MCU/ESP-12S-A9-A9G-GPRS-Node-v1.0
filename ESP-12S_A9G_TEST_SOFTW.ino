5ee47e7292949be8a34b032ec25ed0fc62bd490a
IOT-MCU <32443558+IOT-MCU@users.noreply.github.com>
2019-11-16 22:46:35 +0800

    Add files via upload

---
+++ ESP-12S_A9G_TEST_SOFTW.ino
@@ -0,0 +1,196 @@
+
+#include<stdio.h>
+#include<string.h>
+#define DEBUG true
+
+#include <SoftwareSerial.h>
+
+#define BAUD_RATE 115200
+#define A9_BAUD_RATE 9600
+
+/***********************************/
+#define A9G_PON     16  //ESP12 GPIO16 A9/A9G POWON
+#define A9G_POFF    15  //ESP12 GPIO15 A9/A9G POWOFF
+#define A9G_WAKE    13  //ESP12 GPIO13 A9/A9G WAKE
+#define A9G_LOWP    2  //ESP12 GPIO2 A9/A9G ENTER LOW POWER MODULE
+
+void handleInterrupt();
+int A9GPOWERON();
+
+/***********************************/
+SoftwareSerial swSer(14, 12, false, 256);
+void setup() {
+  Serial.begin(BAUD_RATE);
+  swSer.begin(A9_BAUD_RATE);
+
+  for (char ch = ' '; ch <= 'z'; ch++) {
+    swSer.write(ch);
+  }
+  swSer.println("");
+
+/************************************/
+  pinMode(A9G_PON, OUTPUT);//LOW LEVEL ACTIVE
+  pinMode(A9G_POFF, OUTPUT);//HIGH LEVEL ACTIVE
+  pinMode(A9G_LOWP, OUTPUT);//LOW LEVEL ACTIVE
+
+  digitalWrite(A9G_PON, HIGH); 
+  digitalWrite(A9G_POFF, LOW); 
+  digitalWrite(A9G_LOWP, HIGH); 
+
+  Serial.println("After 2s A9G will POWER ON.");
+  delay(2000);
+  if(A9GPOWERON()==1)
+  {
+       Serial.println("A9G POWER ON.");
+  }
+
+ /********************A9G POWER OFF TEST***********************/
+/*
+  Serial.println("After 5s A9G will POWER OFF.");
+  delay(5000);
+   if(A9GPOWEROFF()==0)
+  {
+       Serial.println("A9G POWER OFF.");
+  }
+
+ Serial.println("After 5s A9G will POWER ON.");
+  delay(5000);
+  if(A9GPOWERON()==1)
+  {
+       Serial.println("A9G POWER ON.");
+  }
+
+*/
+ /******************A9G GPIO LOW POWER TEST****************************/
+ /*
+  Serial.println("After 15s A9G will ENTER GPIO LOW POWR MODULE.");
+  delay(15000);
+ //  Serial.println(A9GENTERLOWPOWER());
+  if(A9GENTERLOWPOWER()==1)
+  {
+       Serial.println("A9G ENTER GPIO LOW POWR MODULE.");
+  }
+
+*/
+/*
+  Serial.println("Enale the GPS");
+  sendData("AT+GPS=1",1000,DEBUG);
+
+  
+  pinMode(A9G_WAKE, INPUT);//interruptPin
+  attachInterrupt(digitalPinToInterrupt(A9G_WAKE), handleInterrupt, RISING);
+*/
+  /*********************GPIO0 TEST********************/
+  /*
+   pinMode(0, INPUT_PULLUP);//interruptPin
+  attachInterrupt(digitalPinToInterrupt(0), handleInterrupt, FALLING);
+ */ 
+
+
+
+
+
+
+
+/***********************************/
+
+
+}
+
+void loop() {
+
+  while (swSer.available() > 0) {
+    Serial.write(swSer.read());
+ //   yield();
+  }
+  while (Serial.available() > 0) {
+    swSer.write(Serial.read());
+//    yield();
+  }
+
+ /* 
+  Serial.print("analogRead:");
+  Serial.println(analogRead(A0));
+  delay(1000);
+  sendData("AT+CCID",1000,DEBUG);
+  delay(1000);
+  sendData("AT+CSQ",1000,DEBUG);
+  delay(1000);
+  sendData("AT+LOCATION=2",1000,DEBUG);
+  delay(1000);
+ */
+}
+
+
+void handleInterrupt() {
+  Serial.println("An interrupt has occurred.");
+}
+
+String sendData(String command, const int timeout, boolean debug)
+{
+    String response = "";    
+    swSer.println(command); 
+    long int time = millis();   
+    while( (time+timeout) > millis())
+    {
+      while(swSer.available())
+      {       
+        char c = swSer.read(); 
+        response+=c;
+      }  
+    }    
+    if(debug)
+    {
+      Serial.print(response);
+    }    
+    return response;
+}
+
+int A9GPOWERON()
+{
+      digitalWrite(A9G_PON, LOW);
+      delay(3000);
+      digitalWrite(A9G_PON, HIGH);
+      delay(5000);
+      String msg = String("");
+      msg=sendData("AT",1000,DEBUG);
+      if( msg.indexOf("OK") >= 0 ){
+          Serial.println("GET OK");
+          return 1;
+      }
+      else {
+         Serial.println("NOT GET OK");
+         return 0;
+      }
+}
+
+int A9GPOWEROFF()
+{
+      digitalWrite(A9G_POFF, HIGH);
+      delay(3000);
+      digitalWrite(A9G_POFF, LOW);
+      delay(5000);
+      String msg = String("");
+      msg=sendData("AT",1000,DEBUG);
+      if( msg.indexOf("OK") >= 0 ){
+          Serial.println("GET OK");
+          return 1;
+      }
+      else {
+          Serial.println("NOT GET OK");
+          return 0;
+      }
+}
+
+int A9GENTERLOWPOWER()
+{
+      String msg = String("");
+      msg=sendData("AT+SLEEP=1",1000,DEBUG);
+      if( msg.indexOf("OK") >= 0 ){
+          digitalWrite(A9G_LOWP, LOW);
+          return 1;
+      }
+      else {
+         return 0;
+      }
+}
